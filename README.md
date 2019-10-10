729404be0365e727f32a9541b724e7e3d2b5bd33
ana.sapata <ana_sapata@sapo.pt>
2019-10-10 14:44:37 +0100

    Adicionada proposta no readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Uso-de-Machine-Learning-para-previs-o-de-doen-as
+# Uso de Machine Learning para previsão de doenças
 Projeto de Machine Learning realizado na disciplina de Projeto Integrado de EI-UÉvora
+
+*Titulo do Projeto:*
+
+**Uso de Machine Learning para previsão de doenças**
+
+
+*Elementos:*
+
+42075 – Raquel Lopes
+
+42255 – Ana Sapata
+
+45414 – José Azevedo
+
+
+*Objetivos:*
+
+Atráves do artigo “Building meaningful machine learning models for disease prediction” (https://shiring.github.io/machine_learning/2017/03/31/webinar_code) cujo seu desenvolvimento foi efetuado em R, pretende-se implementar o mesmo em Python, de modo a:
+* Aprender-se a utilizar a biblioteca Numpy do Python para manipulação dos dados;
+* Aprender-se a utilizar a biblioteca Scikit-Learn entre outras para implementar algoritmos de machine learning.
+
+
+*Calendário/Planificação*
+
+17/Novembro – Exploração dos packages e Importação dos Dados
+
+07/Novembro – Análise Exploratória dos Dados
+
+28/Novembro – Uso dos algoritmos de Regressão
+
+19/Dezembro – Uso dos algoritmos de Classificação
--- venv/NumpyTutorial.py
+++ venv/NumpyTutorial.py
@@ -1,10 +1,49 @@
-#importação da biblioteca numpy
+# importação da biblioteca numpy
 import numpy as np
 
-#criação de array de zeros
-x = np.zeros((2,3))
-print(x)
+# criação de matriz de zeros com 2 linhas e 3 colunas
+x = np.zeros((2, 3))
+print("x = \n" + str(x))
 
+# criação se matriz de zeros com 4 linhas e 5 colunas
 y = np.zeros((4, 5))
-print(y)
+print("\ny = \n" + str(y))
 
+# criação de um array com 10 elementos todos eles 0
+z = np.zeros((1, 10))
+print("\nz = \n" + str(z))
+
+# criação de uma matriz 3x2 com os elementos que queremos
+matriz1 = np.array([[1, 2],
+                    [0, 3.2],
+                    [1, 7]])
+print("\nmatriz1 = \n" + str(matriz1))
+
+#Verificar as dimensões das variaveis/objetos anteriormente criados
+print("\nDimensão da matriz x:" + str(x.shape) +
+      "\nNúmero de linhas:" + str(x.shape[0]) +
+      "\nNúmero de colunas:" + str(x.shape[1]))
+
+print("\nDimensão da matriz y:" + str(y.shape) +
+      "\nNúmero de linhas:" + str(y.shape[0]) +
+      "\nNúmero de colunas:" + str(y.shape[1]))
+#Verificar que está como sendo mattriz de 1x10
+print("\nDimensão do array z:" + str(z.shape) +
+      "\nNúmero de linhas:" + str(z.shape[0]) +
+      "\nNúmero de colunas:" + str(z.shape[1]))
+
+print("\nDimensão da matriz1:" + str(matriz1.shape) +
+      "\nNúmero de linhas:" + str(matriz1.shape[0]) +
+      "\nNúmero de colunas:" + str(matriz1.shape[1]))
+
+# Criação de array com os numeros entre 10 e 30 com passo igual a 5, sendo que o 30 já não entra
+a = np.arange(10, 30, 5)
+print("\na = \n" + str(a))
+
+#Neste o 30 já entra
+b = np.arange(10, 31, 5)
+print("\nb = \n" + str(b))
+
+#Array composto por 9 elementos entre o 0 e o 2
+c = np.linspace(0, 2, 9)
+print("\nc = \n" + str(c))
