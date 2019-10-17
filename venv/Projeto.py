68cbb4a142c21ab9103bddabaddba80a2747daaa
ana.sapata <ana_sapata@sapo.pt>
2019-10-17 10:46:08 +0100

    Tratamento dos dados finalizado

--- venv/Projeto.py
+++ venv/Projeto.py
@@ -47,11 +47,11 @@ null_columns = df.columns[df.isnull().any()]
 # Conta o número de celulas com valores nulos
 print(df[null_columns].isnull().sum())
 
-# Verificar o tipo dos elementos das colunas 2:10 antes de proceder à alteração
+# Verificar o tipo dos elementos das colunas 1:10 antes de proceder à alteração
 print(df.dtypes)
 
 # Passar os elementos das colunas 2:10 para o tipo numerico
-df.iloc[:,1:9] = df.iloc[:,1:9].apply(lambda x: pd.to_numeric(x),1)
+df.iloc[:,1:10] = df.iloc[:,1:10].apply(lambda x: pd.to_numeric(x),1)
 
 # Verificar se os elementos das colunas referidas já se encontram todos em formato numerico
 print(df.dtypes)
@@ -61,24 +61,49 @@ print(df.dtypes)
 # Informar de como deverá ser feito o impute dos dados
 imp = SimpleImputer(missing_values = np.NaN, strategy = 'mean')
 # Verificar o que está a ser aplicado
-imp.fit(df.iloc[:,1:9])
+imp.fit(df.iloc[:,1:10])
 
 # Realizar a transformação dos dados
-df_impute = imp.transform(df.iloc[:,1:9])
+df_impute = imp.transform(df.iloc[:,1:10])
 # Uma vez que o df_impute é do tipo numpy.ndarray é utilizado o metodo savetxt do numpy para
 # guardar os resultados obtidos e verificar que já não existem NaN
 np.savetxt('/home/anasapata/Personal/ProjetoIntegrado/teste.csv', df_impute, delimiter = ";")
 
-# Utilizar outro mmetodo para impute
+# Utilizar outro metodo para impute
 imp2 = IterativeImputer(max_iter = 10, random_state = 0)
-imp2.fit(df.iloc[:,1:9])
-df_impute2 = imp2.transform(df.iloc[:,1:9])
+imp2.fit(df.iloc[:,1:10])
+df_impute2 = imp2.transform(df.iloc[:,1:10])
 np.savetxt('/home/anasapata/Personal/ProjetoIntegrado/teste_2.csv', df_impute2, delimiter = ";")
 
-#df_impute2['classes'] = df_impute2['classes'].astype('category')
-#print( 'Aqui')
-#print(df_impute2)
-#pd.concat(df.iloc[:,0], df_impute2, df.iloc[:,10])
-#ben = df_impute2[df_impute2.classes == 'bening']
-#print(ben.shape)
+# Como o resultado do impute é um numpy ndarray existe a necessidade de passar o mesmo para o formato data frame
+df_impute2_df = pd.DataFrame(data = df_impute2)
 
+# atribuir o nome das colunas aos dados onde foi resultado o impute
+df_impute2_df.columns = ['clump_thickness',
+              'uniformity_of_cell_size',
+              'uniformity_of_cell_shape',
+              'marginal_adhesion',
+              'single_epithelial_cell_size',
+              'bare_nuclei',
+              'bland_chromatin',
+              'normal_nucleoli',
+              'mitosis']
+
+# Converter todas as colunas para inteiro
+df_impute2_df = df_impute2_df.astype('int64')
+
+# Selecionar a ultima coluna do data frame original para se poder efetuar o merge com os dados com o impute
+cf = df.iloc[:,10]
+
+# Colocar todas as data frames a juntar num array
+L = [cf, df_impute2_df]
+# Fazer o merge de todos os dados
+df_final = pd.concat(L, axis = 1)
+
+# Definir a coluna classes como uma variavel categorica
+df_final['classes'] = df_final['classes'].astype('category')
+
+ben = df_final[df_final.classes == 'benign']
+mal = df_final[df_final.classes == 'malignant']
+summary_classes = 'benign     malignant\n' + str(ben.shape[0]) + '        ' + str(mal.shape[0])
+print(summary_classes)
